83d3791d76ec551ad813faea181ad162568f0255
Todd Wolfson <todd@twolfson.com>
2020-10-14 19:32:54 -0700

    Leaving more notes to self

--- magnet-toggle/src/main.cpp
+++ magnet-toggle/src/main.cpp
@@ -3,12 +3,13 @@
 //   https://www.instructables.com/id/ATtiny85-Interrupt-Barebones-Example/
 //   http://www.technoblogy.com/show?KX0
 //   https://forum.arduino.cc/index.php?topic=524949.msg3580423#msg3580423
+// TODO: Remove notes now that we have a deeper understanding of ATtiny code
 // Load in our dependencies
 #include <Arduino.h>
 #include <avr/sleep.h>
 #include <avr/interrupt.h>
 
-// Reference to constants
+// Reference to framework constants
 // https://github.com/vancegroup-mirrors/avr-libc/blob/06cc6ff5e6120b36f1b246871728addee58d3f87/avr-libc/include/avr/io.h#L385-L386
 // ATtiny25/45/85 vars: https://github.com/vancegroup-mirrors/avr-libc/blob/06cc6ff5e6120b36f1b246871728addee58d3f87/avr-libc/include/avr/iotnx5.h
 // ATtiny85 specific vars: https://github.com/vancegroup-mirrors/avr-libc/blob/06cc6ff5e6120b36f1b246871728addee58d3f87/avr-libc/include/avr/iotn85.h
@@ -49,13 +50,13 @@ void loop() {
 }
 
 // When we receive an external interrupt on INT0
-// DEV (pretty confident): PCINT0_vect is pin change interrupt request which is unrelated to INT0 interrupt
-//   We cannot use this vector due to it being disabled in power-down, see 7.1 in datasheet, page 34
-// TODO: Understand why `INT0_vect` doesn't work...
+//   https://github.com/vancegroup-mirrors/avr-libc/blob/06cc6ff5e6120b36f1b246871728addee58d3f87/avr-libc/include/avr/interrupt.h#L134-L142
 //   https://github.com/vancegroup-mirrors/avr-libc/blob/06cc6ff5e6120b36f1b246871728addee58d3f87/avr-libc/include/avr/iotnx5.h#L365
 //   These come from table on p48 of datasheet
 ISR(INT0_vect) {
   // If our interrupt occurred on a rising edge (this runs on both rising and falling), then toggle our LED
+  // TODO: See if we can determine a rising/falling edge
+  // TODO: Also prob use the register for this
   if (digitalRead(INTERRUPT_PIN) == HIGH) {
     ledState = !ledState;
     digitalWrite(LED_PIN, ledState);
